b66b5c73b2c0c03d06de0d5e9e461a06f5a7dd1e
Loc <hoanglocst900@gmail.com>
2021-10-19 02:28:27 +0700

    ADD - add status constants

---
+++ src/constants/statusConstants.ts
@@ -0,0 +1,9 @@
+// 2xx
+export const CREATED = 201;
+export const NO_CONTENT = 204;
+
+// 4xx
+export const BAD_REQUEST = 400;
+export const UNAUTHORIZED = 401;
+export const FORBIDDEN = 403;
+export const NOT_FOUND = 404;
