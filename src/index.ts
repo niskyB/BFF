97e69289f3400bb64af1126e4cdfe9eeb6dde051
Loc <hoanglocst900@gmail.com>
2021-10-21 21:38:53 +0700

    UPDATE - add logging error and error middleware to routes

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@ import * as express from "express";
 import * as dotenv from "dotenv";
 import * as path from "path";
 import routeConfig from "./startup/routes";
-
+import { config } from "./startup/config";
+import logging from "./startup/logging";
 // dotenv config
 dotenv.config({
     path: path.resolve(__dirname, `./config/.env.${process.env.NODE_ENV}`)
@@ -11,8 +12,11 @@ dotenv.config({
 
 // start up
 const app = express();
+logging();
 dbStartUp();
 routeConfig(app);
+config();
+
 
 // listen
 const port = process.env.PORT || 3000;
--- src/startup/routes.ts
+++ src/startup/routes.ts
@@ -1,8 +1,10 @@
 import { Application } from "express";
 import * as express from "express";
 import userRoute from "../routes/userRoute";
+import { error } from "../middlewares/error";
 
 export default function (app: Application) {
     app.use(express.json());
     app.use('/api/user', userRoute);
+    app.use(error);
 }
