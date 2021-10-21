26609e244563a39218f2047e13999d62b0062305
Loc <hoanglocst900@gmail.com>
2021-10-22 03:21:21 +0700

    FIX - fix variable name

--- src/utils/userHelper.ts
+++ src/utils/userHelper.ts
@@ -2,6 +2,6 @@ import * as jwt from "jsonwebtoken";
 import { User } from "../entity/User";
 
 export async function gentoken(user: User) {
-    const token = jwt.sign({ id: user.userId, role: user.roleId }, process.env.JWT_PRIVATE_KEY);
+    const token = jwt.sign({ userId: user.userId, roleId: user.roleId }, process.env.JWT_PRIVATE_KEY);
     return token;
 }
