d9f781dd8a07e9fa29f4f37bf7ab0238353f8858
Loc <hoanglocst900@gmail.com>
2021-10-22 03:23:02 +0700

    UPDATE - add cookie parser

--- src/startup/routes.ts
+++ src/startup/routes.ts
@@ -2,9 +2,11 @@ import { Application } from "express";
 import * as express from "express";
 import userRoute from "../routes/userRoute";
 import { error } from "../middlewares/error";
+import * as cookieParser from "cookie-parser";
 
 export default function (app: Application) {
     app.use(express.json());
+    app.use(cookieParser());
     app.use('/api/user', userRoute);
     app.use(error);
 }
