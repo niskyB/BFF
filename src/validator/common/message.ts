8d5a907ade4bf7768f34833db40664bca45c066e
Loc <hoanglocst900@gmail.com>
2021-10-23 20:45:33 +0700

    UPDATE - custom email and phone messages

--- src/validator/common/message.ts
+++ src/validator/common/message.ts
@@ -1,10 +1,16 @@
 export const stringCustomMessage = {
     "string.empty": "required",
     "any.required": "required",
-    "string.min": "length should be at least 3 characters"
+    "string.min": "length should be at least 3 characters",
+    "any.only": "should match with password"
 };
 
 export const stringCustomEmail = {
     ...stringCustomMessage,
     "string.email": "invalid email"
 };
+
+export const stringCustomPhone = {
+    ...stringCustomMessage,
+    "string.pattern.base": "invalid phone"
+}
